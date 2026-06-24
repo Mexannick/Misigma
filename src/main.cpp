e8f40e47e4389906043876700d83d0751b662eb9
agenteec <g.mens.hl@gmail.com>
2026-06-25 00:58:06 +0300

    Win main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,87 @@
-#include "wiApplication.h"
-#include "wiRenderPath3D.h"
+#include "WickedEngine.h"
 
-class Game : public wi::Application
+wi::Application application;
+
+int APIENTRY wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
-    wi::RenderPath3D renderPath;
-
-public:
-    void Initialize() override
+    static auto WndProc = [](HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) -> LRESULT
     {
-        wi::Application::Initialize();
+        switch (message)
+        {
+        case WM_SIZE:
+        case WM_DPICHANGED:
+            if (application.is_window_active)
+                application.SetWindow(hWnd);
+            break;
+        case WM_CHAR:
+            switch (wParam)
+            {
+            case VK_BACK:
+                wi::gui::TextInputField::DeleteFromInput();
+                break;
+            default:
+                wi::gui::TextInputField::AddInput((wchar_t)wParam);
+                break;
+            }
+            break;
+        case WM_INPUT:
+            wi::input::rawinput::ParseMessage((void*)lParam);
+            break;
+        case WM_KILLFOCUS:
+            application.is_window_active = false;
+            break;
+        case WM_SETFOCUS:
+            application.is_window_active = true;
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        return 0;
+    };
 
-        renderPath.Load();
-        ActivatePath(&renderPath);
-    }
-};
+    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    Game game;
+    WNDCLASSEXW wcex = {};
+    wcex.cbSize        = sizeof(WNDCLASSEX);
+    wcex.style         = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc   = WndProc;
+    wcex.hInstance     = hInstance;
+    wcex.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszClassName = L"MisigmaWindow";
+    RegisterClassExW(&wcex);
+
+    HWND hWnd = CreateWindowW(
+        wcex.lpszClassName, L"Misigma",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0,
+        nullptr, nullptr, hInstance, nullptr);
+    ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+    application.SetWindow(hWnd);
     wi::arguments::Parse(lpCmdLine);
-    game.Run();
-    return 0;
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            application.Run();
+        }
+    }
+
+    wi::jobsystem::ShutDown();
+    return (int)msg.wParam;
 }
